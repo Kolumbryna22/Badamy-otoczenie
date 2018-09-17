382e114f93ae24f42e0a12277129afd57dd8bdd6
Dominik Wilusz <wilusz1@o2.pl>
2018-09-17 11:08:44 +0200

    Finish task

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ fs.readdir('./pliki', 'utf-8', function(err, data) {
     var i;
 
     for (i = 0; i < data.length; i++) {
-        fs.appendFile('./text.txt', ('\n' + getData(data[i])), function(err) {
-            if (err) throw err;
-        });
+        getData(data[i], inputData);
     }
 });
 
-function getData(doc) {
-    var text;
+function inputData(text) {
+    fs.appendFile('./text.txt', ('\n' + text), function(err) {
+        if (err) throw err;
+    });
+};
 
+function getData(doc, next) {
     fs.readFile('./pliki/' + doc, 'utf-8', function(err, data) {
         if (err) throw err;
-        text = data;
-    });
-    
-    console.log(text);
 
-    return text;
+        next(data);
+    });
 };
